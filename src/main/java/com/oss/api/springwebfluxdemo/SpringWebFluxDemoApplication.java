861eb5bf5f273f9cf588587fe65588f428ad92ed
오성식(**1213) / Logistics플랫폼담당 WMS팀 <221213@ssg.com>
2022-03-26 20:16:19 +0900

    first commit

---
+++ src/main/java/com/oss/api/springwebfluxdemo/SpringWebFluxDemoApplication.java
@@ -0,0 +1,13 @@
+package com.oss.api.springwebfluxdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebFluxDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebFluxDemoApplication.class, args);
+	}
+
+}
