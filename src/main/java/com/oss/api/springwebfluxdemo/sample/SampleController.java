6bc074b3bc4339cbcce2b1c3a7f2a51cf050b765
오성식 <oss0202@naver.com>
2022-03-29 20:16:19 +0900

    10가지 1씩 증가하는 스트림 반환

--- src/main/java/com/oss/api/springwebfluxdemo/sample/SampleController.java
+++ src/main/java/com/oss/api/springwebfluxdemo/sample/SampleController.java
@@ -1,5 +1,8 @@
 package com.oss.api.springwebfluxdemo.sample;
 
+import java.util.Collections;
+import java.util.Map;
+import java.util.stream.Stream;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
@@ -9,5 +12,12 @@ public class SampleController {
 	@GetMapping("/")
 	Flux<String> sample(){
 		return Flux.just("Hello", "World");
+	}
+
+	@GetMapping("/stream")
+	Flux<Map<String, Integer>> stream(){
+		Stream<Integer> integerStream = Stream.iterate(0, i -> i + 1); // 1씩 증가하는 무한 스트림
+		return Flux.fromStream(integerStream.limit(10))
+				.map(integer -> Collections.singletonMap("value", integer));
 	}
 }
